6b51dc7ea3d342246a21ff26db2cc7ef284d2cbd
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-08 15:55:47 -0400

    Create module to check if there's a winner

---
+++ check_winner.rb
@@ -0,0 +1,21 @@
+module CheckWinner
+
+  def self.board_check
+    check_rows
+    check_columns
+    check_diag
+  end
+
+  def check_rows
+
+  end
+
+  def check_columns
+
+  end
+
+  def check_diag
+
+  end
+
+end
